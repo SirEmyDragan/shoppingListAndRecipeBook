17e25f2f2cfb66606667c19fa2e733e17fa9eabb
Emi <paul.dragan@ibm.com>
2019-03-22 14:33:28 +0200

    recipe-detail recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,10 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://hips.hearstapps.com/del.h-cdn.co/assets/17/34/2048x1365/gallery-1503418862-chicken-thighs-delish.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test',
+    'https://hips.hearstapps.com/del.h-cdn.co/assets/17/34/2048x1365/gallery-1503418862-chicken-thighs-delish.jpg'),
+    new Recipe('A Test Recipe2', 'This is simply a test',
+    'https://hips.hearstapps.com/del.h-cdn.co/assets/17/34/2048x1365/gallery-1503418862-chicken-thighs-delish.jpg')
   ];
 
   constructor() { }
